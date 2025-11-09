b523c2ec2f2f1973cb3538c61f66962d094f8f93
AaronOwensSE <aaron.owens.se@gmail.com>
2025-11-09 15:56:26 -0600

    frontend: Add dashboard-level screens.

---
+++ screens/Drafts.js
@@ -0,0 +1,98 @@
+"use strict";
+
+import { View, ScrollView, StyleSheet } from "react-native";
+
+import Dashboard from "../components/Dashboard.js";
+import H1 from "../components/headings/H1.js";
+import Card from "../components/cards/Card.js";
+import TitleRow from "../components/cards/TitleRow.js";
+import TextRow from "../components/cards/TextRow.js";
+
+const Drafts = () => {
+    return(
+	<View style={draftStyles.screenView}>
+		<Dashboard style={draftStyles.dashboard} />
+		
+		<ScrollView
+            style={draftStyles.scrollView}
+            contentContainerStyle={draftStyles.scrollViewContentContainer}
+        >
+			<H1 style={draftStyles.h1} text="Drafts" />
+            
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={draftStyles.card}>
+                <TitleRow style={draftStyles.titleRow} titleText="Group Name" tagText="Draft ID" />
+                <TextRow style={draftStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+		</ScrollView>
+	</View>
+    );
+};
+
+export default Drafts;
+
+const draftStyles = StyleSheet.create({
+	screenView: {
+		flex: 1,
+		backgroundColor: "white"
+	},
+	
+	dashboard: {
+		marginTop: 30
+	},
+	
+	scrollView: {
+	},
+	
+	scrollViewContentContainer: {
+		alignItems: "center"
+	},
+	
+	h1: {
+	},
+
+    card: {
+        width: "85%",
+        margin: 5
+    },
+
+    titleRow: {
+    },
+
+    textRow: {
+    }
+});
---
+++ screens/Schedules.js
@@ -0,0 +1,119 @@
+"use strict";
+
+import { View, ScrollView, StyleSheet } from "react-native";
+
+import Dashboard from "../components/Dashboard.js";
+import H1 from "../components/headings/H1.js";
+import H2 from "../components/headings/H2.js";
+import Card from "../components/cards/Card.js";
+import TitleRow from "../components/cards/TitleRow.js";
+import TextRow from "../components/cards/TextRow.js";
+
+const Schedules = () => {
+    return(
+	<View style={scheduleStyles.screenView}>
+		<Dashboard style={scheduleStyles.dashboard} />
+		
+		<ScrollView
+            style={scheduleStyles.scrollView}
+            contentContainerStyle={scheduleStyles.scrollViewContentContainer}
+        >
+			<H1 style={scheduleStyles.h1} text="Schedules" />
+            <H2 style={scheduleStyles.h2} text="Current" />
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <H2 style={scheduleStyles.h2} text="Upcoming" />
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+
+            <Card style={scheduleStyles.card}>
+                <TitleRow
+                    style={scheduleStyles.titleRow}
+                    titleText="Group Name"
+                    tagText="Schedule ID"
+                />
+                <TextRow style={scheduleStyles.textRow} leftText="00/00/0000 to 00/00/0000" />
+            </Card>
+		</ScrollView>
+	</View>
+    );
+};
+
+export default Schedules;
+
+const scheduleStyles = StyleSheet.create({
+	screenView: {
+		flex: 1,
+		backgroundColor: "white"
+	},
+	
+	dashboard: {
+		marginTop: 30
+	},
+	
+	scrollView: {
+	},
+	
+	scrollViewContentContainer: {
+		alignItems: "center"
+	},
+	
+	h1: {
+	},
+
+    h2: {
+    },
+
+    card: {
+        width: "85%",
+        margin: 5
+    },
+
+    titleRow: {
+    },
+
+    textRow: {
+    }
+});
