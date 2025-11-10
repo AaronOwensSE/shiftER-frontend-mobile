d7f861dc1b69040b7c65c1d453dadf567a4cd81d
AaronOwensSE <aaron.owens.se@gmail.com>
2025-11-09 23:17:34 -0600

    frontend: Add and update components.

---
+++ components/navigation/Dashboard.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import { View, StyleSheet } from "react-native";
+
+import SmallLogo from "../logos/SmallLogo.js";
+import NavBar from "./NavBar.js";
+
+const Dashboard = ({style}) => {
+    return(
+        <View style={[ dashboardStyles.view, style ]}>
+            <SmallLogo style={dashboardStyles.smallLogo} />
+            <NavBar style={dashboardStyles.navBar} />
+        </View>
+    );
+};
+
+export default Dashboard;
+
+const dashboardStyles = StyleSheet.create({
+    view: {
+    },
+
+    smallLogo: {
+        width: "100%"
+    },
+
+    navBar: {
+        width: "100%"
+    }
+});
---
+++ components/navigation/DraftNavBar.js
@@ -0,0 +1,48 @@
+"use strict";
+
+import { View, ScrollView, StyleSheet } from "react-native";
+
+import ShifterButton from "../ShifterButton";
+
+const DraftNavBar = ({style}) => {
+    // Outer View helps control weird positioning behavior of ScrollView.
+    return(
+        <View style={[ draftNavBarStyles.view, style ]}>
+            <ScrollView
+                style={draftNavBarStyles.scrollView}
+                contentContainerStyle={draftNavBarStyles.scrollViewContentContainer}
+                horizontal
+            >
+                <ShifterButton style={draftNavBarStyles.firstShifterButton} text="Draft" />
+                <ShifterButton style={draftNavBarStyles.lastShifterButton} text="Participation" />
+            </ScrollView>
+        </View>
+    );
+};
+
+export default DraftNavBar;
+
+const draftNavBarStyles = StyleSheet.create({
+    view: {
+    },
+
+    scrollView: {
+    },
+
+    scrollViewContentContainer: {
+        flex: 1
+    },
+
+    firstShifterButton: {
+        borderRightWidth: 1,
+    },
+
+    midShifterButton: {
+        borderLeftWidth: 1,
+        borderRightWidth: 1
+    },
+
+    lastShifterButton: {
+        borderLeftWidth: 1
+    }
+});
